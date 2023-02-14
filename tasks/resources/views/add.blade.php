8eca74bf9caf5c401e4e43c7f19dc9ee822b4d22
zeevKremer <122747087+zeevKremer@users.noreply.github.com>
2023-02-14 02:15:47 +0200

    Add files via upload

---
+++ tasks/resources/views/add.blade.php
@@ -0,0 +1,39 @@
+<center>
+<style>
+    body
+    {
+        direction:rtl;
+        background:#b8cef6;
+    }
+    h1
+    {
+        color:blue;
+    }    
+    .btn
+    {
+        color:white;
+        background-color:black;
+        height:40px;
+        cursor: pointer;
+        font-size:14px;
+    }    
+    textarea
+    {
+    direction:rtl;
+    color:#0026ff;
+    font-size:24px;
+    text-align:center;
+    background-color:#7ad8c7;
+    }
+
+</style>
+<form action="add" method="GET">
+     
+
+
+    <h1>תיאור המשימה</h1>
+    <br>
+    <textarea rows="10" cols="50" name="textarea2"></textarea><br><br>    
+    <input class="btn" type="submit" name="button1" value="אישור">
+</form>
+</center>
--- tasks/resources/views/delete.blade.php
+++ tasks/resources/views/delete.blade.php
@@ -1,18 +1,16 @@
-<?php
-//קבועים להתקשרות עם בסיס הנתונים
-defined('DB_HOST')? null : define('DB_HOST', 'localhost');
-defined('DB_USER')? null : define('DB_USER', 'root');
-defined('DB_PASS')? null : define('DB_PASS', '');
-defined('DB_NAME')? null : define('DB_NAME', 'dbtasks');
-$connection = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
-$id = $_GET["delete"];
-$sql = "update  tasks set isActive = 0 where taskId = ".$id;
-mysqli_query($connection,$sql);
-mysqli_close($connection);    
-?>
-<center>
-    <style>
-        body
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <style>
+                body
     {
         direction:rtl;
         background:#b8cef6;
@@ -35,10 +33,14 @@ mysqli_close($connection);
         color: #00ff21;
         text-decoration: none;
     }
-    </style>
-    <br><br>
-    <h1>המשימה נמחקה בהצלחה</h1>
-    <br><br>
-    <a href="index.php">חזרה לדף המשימות</a>
-</center>
-
+        </style>
+      
+</head>      
+<body class="antialiased">      
+    <center>
+        <br><br>
+        <h1>המשימה נמחקה בהצלחה</h1>
+        <br><br>
+        <a href="index.php">חזרה לדף המשימות</a>
+    </center>
+</body
